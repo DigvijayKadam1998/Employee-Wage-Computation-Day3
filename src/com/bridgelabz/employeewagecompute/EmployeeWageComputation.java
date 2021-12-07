65c675be9e64dce2e042a45143498d12f48bb975
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-07 11:03:05 +0530

    Added: Calculate Part Time Employee Wage

--- src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
@@ -4,21 +4,26 @@ import java.util.Random;
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		
+
 		Random rd = new Random();
-		int empCheck = rd.nextInt(2);
-		
+		int empCheck = rd.nextInt(3);
+
 		if(empCheck==IS_FULL_TIME) {
-		    empHrs=8;
-		    System.out.println("Employee is Present");
+		    empHrs = 8;
+		    System.out.println("Employee is Full Time");
+		}
+		else if(empCheck==IS_PART_TIME) {
+		    empHrs = 4;
+		    System.out.println("Employee is Part Time");
 		}
 		else {
 		    System.out.println("Employee is Absent");
 		}
-		empWage= WAGE_PER_HOUR * empHrs;
+		empWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Daily Wage of Employee is: " + empWage);
 	}
 }
