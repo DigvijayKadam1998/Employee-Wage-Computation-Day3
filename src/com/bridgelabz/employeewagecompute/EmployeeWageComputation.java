aa5ea99692841b1636c92033dc5bd383eae14990
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-07 10:49:54 +0530

    Added: Employee Attendance

--- src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewagecompute/EmployeeWageComputation.java
@@ -1,7 +1,20 @@
 package com.bridgelabz.employeewagecompute;
 
+import java.util.Random;
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Problem");
+		// declare constant variable
+		int IS_PRESENT = 1;
+		// create object of random class
+		Random rd = new Random();
+		// generating number 0 or 1
+		int empcheck =rd.nextInt(2);
+		// applying condition for checking present or absent by using if statement
+		if(empcheck==IS_PRESENT) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
